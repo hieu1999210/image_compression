fix entropy calculation

modelling/blocks/entropy_model.py
@@ -135,13 +135,11 @@ class BaseEntropyModel(nn.Module):
         In end-to-end training, the loss function simutaneously learning q as well as minimize
         entropy of p  (H(p,q) = KL(p || q) + H(p))
         
-        NOTE: H is computed as mean per pixel in base 2 in order to make loss scale independent
-            from batch size and image size 
+        NOTE: H is computed as sum overall pixel and in base 2
         """
         
         # print(probs.min(), probs.max(), torch.isnan(torch.log(probs + 1e-5)).any(), probs.size())
-        N,C, *_ = probs.size()
-        return (torch.clamp(-1.0 * torch.log(probs + 1e-10) / LOG2, 0, 50)).mean() * C
+        return (torch.clamp(-1.0 * torch.log(probs + 1e-10) / LOG2, 0, 50)).sum()
 
 
 @ENTROPY_MODEL_REGISTRY.register()

modelling/meta_arch/bmshl2018.py
@@ -25,9 +25,11 @@ class Compressor2018(nn.Module):
             cfg.MODEL.ENTROPY_MODEL.CONDITIONAL_MODEL)(cfg)
         self.mse_loss               = nn.MSELoss()
         self.distortion_loss_weight = cfg.MODEL.LOSS.DISTORTION_LOSS_WEIGHT
-        self.loss_names             = ["y_entropy", "z_entropy", "distortion"]
+        self.loss_names             = ["y_entropy", "z_entropy", "distortion", "bpp"]
         
     def forward(self, x):
+        N,C,H,W = x.size()
+        num_pixels = N*H*W
         y = self.analysis_transform(x)
         # print("y", y.shape)
         z = self.prior_analysis(torch.abs(y))
@@ -48,12 +50,13 @@ class Compressor2018(nn.Module):
         mse_loss = self.mse_loss(x, x_tilde)
         
         # entropy loss
-        entropy_loss = z_ce_loss + y_ce_loss
+        entropy_loss = (z_ce_loss + y_ce_loss) / num_pixels
         
         total_loss = self.distortion_loss_weight*mse_loss + entropy_loss
         return x_tilde.detach(), {
-            "z_entropy": z_ce_loss.detach(),
-            "y_entropy": y_ce_loss.detach(),
+            "z_entropy": z_ce_loss.detach() / num_pixels,
+            "y_entropy": y_ce_loss.detach() / num_pixels,
+            "bpp": entropy_loss.detach(),
             "distortion": mse_loss.detach(),
             "total_loss": total_loss,
         }