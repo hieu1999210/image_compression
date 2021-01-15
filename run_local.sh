f7ef8e292341f228f3c992627f6f950cc11e9816
hieu <hieupro1999210@gmail.com>
2021-01-15 09:50:15 +0700

    minor change

--- run_local.sh
+++ run_local.sh
@@ -4,7 +4,11 @@
     # --resume
 
 CUDA_VISIBLE_DEVICES=0 python main.py \
-    --config '/home/hieu/lib/image_compression/configs/debug5.yaml' \
-    --load '/mnt/HDD/runs__/debug5/checkpoints/iter_00131072_31.0966.pth' \
+    --config '/home/hieu/lib/image_compression/configs/ssim_512.yaml' \
+    --load '/mnt/HDD/runs__/ssim_512/checkpoints/iter_00129792_24.2206.pth' \
     --valid \
-    # --save_output \
+    --save_output \
+
+
+# /mnt/HDD/runs__/psnr_8192/checkpoints/iter_00109824_37.6322.pth
+# /mnt/HDD/runs__/psnr_256/checkpoints/iter_00130816_28.8680.pth
